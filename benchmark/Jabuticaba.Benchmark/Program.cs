f2ec4e90520af9c568dfe694fdf98fa4cf0872ea
Feranando Gonçalves <hd1.fernandosouza@hotmail.com>
2020-12-24 20:54:20 -0300

    test: criaçao de projeto para fazer testes te benchmark

---
+++ benchmark/Jabuticaba.Benchmark/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Jabuticaba.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CpfBenchmark.ValidarCpf(numeroTentativas: 1_000_000);
+        }
+    }
+
+    public static class CpfBenchmark
+    {
+        public static void ValidarCpf(ulong numeroTentativas)
+        {
+            var stopWatch = new Stopwatch();
+            var gcAntesGeracao2 = GC.CollectionCount(2);
+            var gcAntesGeracao1 = GC.CollectionCount(1);
+            var gcAntesGeracao0 = GC.CollectionCount(0);
+
+            stopWatch.Start();
+            for (ulong i = 0; i < numeroTentativas; i++)
+            {
+                Cpf cpf = "529.982.247-25";
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine($"Quantidade de cpfs {numeroTentativas}");
+            Console.WriteLine($"Tempo execução: {stopWatch.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC geração 2 - {GC.CollectionCount(2) - gcAntesGeracao2}");
+            Console.WriteLine($"GC geração 1 - {GC.CollectionCount(1) - gcAntesGeracao1}");
+            Console.WriteLine($"GC geração 0 - {GC.CollectionCount(0) - gcAntesGeracao0}");
+        }
+    }
+}
