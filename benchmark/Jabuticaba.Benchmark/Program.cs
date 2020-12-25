8e21395fa28bd942677690bbec45c1b7aa383eec
Feranando Gonçalves <hd1.fernandosouza@hotmail.com>
2020-12-25 08:46:33 -0300

    tests: permissao de unsafe para testes te peformace e melhoria na descricao

--- benchmark/Jabuticaba.Benchmark/Program.cs
+++ benchmark/Jabuticaba.Benchmark/Program.cs
@@ -13,6 +13,14 @@ namespace Jabuticaba.Benchmark
 
     public static class CpfBenchmark
     {
+        /***
+        Último resultado alcançado:
+        Quantidade de cpfs 1000000
+        Tempo execução: 527ms
+        GC geração 2 - 0
+        GC geração 1 - 0
+        GC geração 0 - 45
+        */
         public static void ValidarCpf(ulong numeroTentativas)
         {
             var stopWatch = new Stopwatch();
@@ -27,11 +35,13 @@ namespace Jabuticaba.Benchmark
             }
             stopWatch.Stop();
 
+            Console.WriteLine($"{nameof(Cpf)}:");
             Console.WriteLine($"Quantidade de cpfs {numeroTentativas}");
             Console.WriteLine($"Tempo execução: {stopWatch.ElapsedMilliseconds}ms");
             Console.WriteLine($"GC geração 2 - {GC.CollectionCount(2) - gcAntesGeracao2}");
             Console.WriteLine($"GC geração 1 - {GC.CollectionCount(1) - gcAntesGeracao1}");
             Console.WriteLine($"GC geração 0 - {GC.CollectionCount(0) - gcAntesGeracao0}");
+            Console.WriteLine();
         }
     }
 }
