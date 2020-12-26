4960ebf2abd5968af6fcea221476eb01569e6371
Feranando Gonçalves <hd1.fernandosouza@hotmail.com>
2020-12-26 09:14:31 -0300

    feat: tipo cnpj implementado

--- src/Jabuticaba/Cnpj.cs
+++ src/Jabuticaba/Cnpj.cs
@@ -18,8 +18,18 @@ namespace Jabuticaba
         public void Validar()
         {
             Span<int> stackCnpj = stackalloc int[14];
+            ValidarSeNulo();
             ValidarSeSomentDigito();
             ValidarTamanho();
+            RemoverMascara(stackCnpj);
+            ValidarPrimeroDigito(stackCnpj);
+            ValidarSegundoDigito(stackCnpj);
+        }
+
+        private void ValidarSeNulo()
+        {
+            if (_cnpj is null)
+                throw new NullReferenceException("O CNPJ não pode ser nulo");
         }
 
         private void ValidarSeSomentDigito()
@@ -41,8 +51,76 @@ namespace Jabuticaba
                 if (char.IsDigit(c))
                     contador++;
             }
-            if(contador is not 14)
+            if (contador is not 14)
                 throw new CnpjInvalidoException($"O CNPJ deve ter 14 dígitos. {contador} dígitos foram informados");
         }
+
+        private void RemoverMascara(Span<int> cnpjSpan)
+        {
+            int contador = 0;
+            foreach (var c in _cnpj)
+            {
+                if (char.IsDigit(c))
+                    cnpjSpan[contador++] = c - 0x30;
+            }
+        }
+
+        private void ValidarPrimeroDigito(Span<int> cnpj)
+        {
+            int resultado =
+                cnpj[0] * 5 +
+                cnpj[1] * 4 +
+                cnpj[2] * 3 +
+                cnpj[3] * 2 +
+                cnpj[4] * 9 +
+                cnpj[5] * 8 +
+                cnpj[6] * 7 +
+                cnpj[7] * 6 +
+                cnpj[8] * 5 +
+                cnpj[9] * 4 +
+                cnpj[10] * 3 +
+                cnpj[11] * 2;
+
+            int mod = resultado % 11;
+            int digitoVerificador;
+
+            if (mod < 2)
+                digitoVerificador = 0;
+            else
+                digitoVerificador = 11 - mod;
+
+            if (cnpj[12] != digitoVerificador)
+                throw new CnpjInvalidoException($"O CNPJ {_cnpj} é inválido.");
+        }
+
+        private void ValidarSegundoDigito(Span<int> cnpj)
+        {
+            int resultado =
+                cnpj[0] * 6 +
+                cnpj[1] * 5 +
+                cnpj[2] * 4 +
+                cnpj[3] * 3 +
+                cnpj[4] * 2 +
+                cnpj[5] * 9 +
+                cnpj[6] * 8 +
+                cnpj[7] * 7 +
+                cnpj[8] * 6 +
+                cnpj[9] * 5 +
+                cnpj[10] * 4 +
+                cnpj[11] * 3 +
+                cnpj[12] * 2;
+
+            int mod = resultado % 11;
+            int digitoVerificador;
+
+            if (mod < 2)
+                digitoVerificador = 0;
+            else
+                digitoVerificador = 11 - mod;
+
+            if (cnpj[13] != digitoVerificador)
+                throw new CnpjInvalidoException($"O CNPJ {_cnpj} é inválido.");
+        }
+
     }
 }
