c5287c89bd6c4ae614a919a102baa81eb4872bba
Feranando Gonçalves <hd1.fernandosouza@hotmail.com>
2020-12-27 22:35:17 -0300

    tests: adiçao de novos testes de benchmark validando todas possiblidades de cnpj

--- benchmark/Jabuticaba.Benchmark/Program.cs
+++ benchmark/Jabuticaba.Benchmark/Program.cs
@@ -10,8 +10,8 @@ namespace Jabuticaba.Benchmark
         static void Main(string[] args)
         {
 #if RELEASE
-            var summaryCpf = BenchmarkRunner.Run<CpfBenchmarkDiagnoser>();
-            // var summaryCnpj = BenchmarkRunner.Run<CnpjBenchmarkDiagnoser>();
+            // var summaryCpf = BenchmarkRunner.Run<CpfBenchmarkDiagnoser>();
+            var summaryCnpj = BenchmarkRunner.Run<CnpjBenchmarkDiagnoser>();
 #endif
 
             List<IBenchmarkLocal> benchmarks = new() { new CnpjBenchmark(), new CpfBenchmark() };
@@ -20,71 +20,60 @@ namespace Jabuticaba.Benchmark
             );
         }
     }
+
     [RankColumn]
     [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     [MemoryDiagnoser]
-    public class CpfBenchmarkDiagnoser
-    {
-        [Benchmark]
-        public void ObterCpf()
-        {
-            Cpf cpf = "529.982.247-25";
-            cpf.Validar();
-        }
-
-
-        [Benchmark]
-        public void CpfPrimeiroDigitoInvalido()
-        {
-
-            Cpf cpf = "149.764.610-00";
-            cpf.Validar(ModoCascateamento.PararNoPrimeiroErro);
-        }
-
-        [Benchmark]
-        public void CpfSegundoDigitoInvalido()
-        {
-
-            Cpf cpf = "529.982.247-20";
-            cpf.Validar();
-        }
-
-        [Benchmark]
-        public void CpfApenasDigitosRepetidos()
-        {
-            Cpf cpf = "111.111.111-11";
-            cpf.Validar();
-        }
-
-        [Benchmark]
-        public void CpfTamanhoMaiorDoQue11Digitos()
-        {
-            Cpf cpf = "149.764.610-331";
-            cpf.Validar(ModoCascateamento.PararNoPrimeiroErro);
-        }
-
-        [Benchmark]
-        public void CpfTamanhoMenorDoQue11Digitos()
-        {
-            Cpf cpf = "149.764.610";
-            cpf.Validar();
-        }
-
-        [Benchmark]
-        public void CpfContendoValorNaoNumerico()
-        {
-            Cpf cpf = "149.764.610a";
-            cpf.Validar();
-        }
-    }
-
-    [MemoryDiagnoser]
     public class CnpjBenchmarkDiagnoser
     {
-        [Benchmark(Baseline = true)]
+        [Benchmark]
         public void ObterCnpj()
         {
             Cnpj cnpj = "78.322.994/0001-50";
+            cnpj.Validar();
         }
+
+        [Benchmark]
+        public void CnpjSemMascara()
+        {
+            Cnpj cnpj = "78322994000150";
+            cnpj.Validar();
+        }
+
+        [Benchmark]
+        public void CnpjValorNaoNumerico()
+        {
+            Cnpj cnpj = "2.055.097/0001-6a";
+            cnpj.Validar();
+        }
+
+        [Benchmark]
+        public void CnpjMenorDoQue14Digitos()
+        {
+            Cnpj cnpj = "02.055.097/0001-6";
+            cnpj.Validar();
+        }
+
+        [Benchmark]
+        public void CnpjMaiorDoQue14Digitos()
+        {
+            Cnpj cnpj = "02.055.097/0001-656";
+            cnpj.Validar();
+        }
+
+        [Benchmark]
+        public void CnpjPrimeiroDigitoInvalido()
+        {
+            Cnpj cnpj = "02.055.097/0001-05";
+            cnpj.Validar();
+        }
+
+        [Benchmark]
+        public void CnpjSegundoDigitoInvalido()
+        {
+            Cnpj cnpj = "02.055.097/0001-60";
+            cnpj.Validar();
+        }
+
     }
 }
